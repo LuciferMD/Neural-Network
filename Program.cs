634e3fb2042fdd8581a77fb68af9be47cac871f9
Dmitry <dima.fenix@list.ru>
2022-05-30 21:18:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Neural_Network
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
